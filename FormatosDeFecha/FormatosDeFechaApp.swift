fd0685b675d1c61eff42a8c419a0c8f4716d14bd
Gersai <gersai_ventura@hotmail.com>
2023-08-23 11:20:57 -0600

    Initial Commit

---
+++ FormatosDeFecha/FormatosDeFechaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormatosDeFechaApp.swift
+//  FormatosDeFecha
+//
+//  Created by Eduardo Gersai Garcia Ventura on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FormatosDeFechaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
